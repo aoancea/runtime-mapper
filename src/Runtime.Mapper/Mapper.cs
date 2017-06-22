66f8e9d5f520d39764667ab7eff12fca49c45d59
Alex Oancea <alx.oancea@gmail.com>
2017-06-22 04:57:58 +0300

    mapper interfaces

---
+++ src/Runtime.Mapper/Mapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Runtime.Mapper
+{
+    public static class Mapper
+    {
+        private static Dictionary<Tuple<Type, Type>, Func<object, object, object>> mappingFunctions = new Dictionary<Tuple<Type, Type>, Func<object, object, object>>();
+
+        public static TDestination DeepCopyTo<TDestination>(this object source)
+        {
+            return default(TDestination);
+        }
+
+        public static void Map<TSource, TDestination>(TSource source, TDestination destination)
+        {
+
+        }
+    }
+}
