e317052b7e01c3bde30231bdc61a1955d0f90ace
aoancea <alx.oancea@gmail.com>
2017-06-24 11:21:05 +0300

    store and get mapping function

--- src/Runtime.Mapper/Mapper.cs
+++ src/Runtime.Mapper/Mapper.cs
@@ -1,20 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace Runtime.Mapper
 {
     public static class Mapper
     {
-        private static Dictionary<Tuple<Type, Type>, Func<object, object, object>> mappingFunctions = new Dictionary<Tuple<Type, Type>, Func<object, object, object>>();
+        private static ConcurrentDictionary<Tuple<Type, Type>, Func<object, object, object>> mappingFunctions = new ConcurrentDictionary<Tuple<Type, Type>, Func<object, object, object>>();
 
         public static TDestination DeepCopyTo<TDestination>(this object source)
         {
-            return default(TDestination);
+            Type sourceType = source.GetType();
+            Type destinationType = typeof(TDestination);
+
+            Tuple<Type, Type> key = new Tuple<Type, Type>(sourceType, destinationType);
+
+            Func<object, object, object> mappingFunction = mappingFunctions.GetOrAdd(key, GetMappingFunction());
+
+            return (TDestination)mappingFunction(source, null);
         }
 
         public static void Map<TSource, TDestination>(TSource source, TDestination destination)
         {
+            Type sourceType = source.GetType();
+            Type destinationType = typeof(TDestination);
 
+            Tuple<Type, Type> key = new Tuple<Type, Type>(sourceType, destinationType);
+
+            Func<object, object, object> mappingFunction = mappingFunctions.GetOrAdd(key, GetMappingFunction());
+
+            mappingFunction(source, destination);
+        }
+
+
+        private static Func<object, object, object> GetMappingFunction()
+        {
+            return null;
         }
     }
 }
