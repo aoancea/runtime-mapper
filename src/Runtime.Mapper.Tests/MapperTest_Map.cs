252dc0d375c952439f81e4632adbf39d6853f295
Alex Oancea <alx.oancea@gmail.com>
2017-06-26 00:00:52 +0300

    test(mapper): consolidate test classes

--- src/Runtime.Mapper.Tests/MapperTest_Map.cs
+++ src/Runtime.Mapper.Tests/MapperTest_Map.cs
@@ -11,8 +11,8 @@ namespace Runtime.Mapper.Tests
         {
             Cow source = new Cow()
             {
-                Id = new Guid("80415b86-1cd0-4a11-83e5-d228d3a60354"),
-                Name = "Cow 1"
+                Guid = new Guid("80415b86-1cd0-4a11-83e5-d228d3a60354"),
+                String = "Cow 1"
             };
 
             Cow destination = new Cow();
@@ -23,8 +23,8 @@ namespace Runtime.Mapper.Tests
             Assert.AreEqual(destination, destination);
             Assert.AreNotEqual(source, destination);
 
-            Assert.AreEqual(new Guid("80415b86-1cd0-4a11-83e5-d228d3a60354"), destination.Id);
-            Assert.AreEqual("Cow 1", destination.Name);
+            Assert.AreEqual(new Guid("80415b86-1cd0-4a11-83e5-d228d3a60354"), destination.Guid);
+            Assert.AreEqual("Cow 1", destination.String);
         }
     }
 }
---
+++ src/Runtime.Mapper.Tests/Models.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Runtime.Mapper.Tests
+{
+    internal class BaseClass
+    {
+        public int Int { get; set; }
+
+        public decimal Decimal { get; set; }
+
+        public string String { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public Guid Guid { get; set; }
+
+        public bool Bool { get; set; }
+
+        //public Dictionary<Guid, string> Dictionary { get; set; }
+
+        #region Nullable primitives
+
+        //public int? IntNullable { get; set; }
+
+        //public decimal? DecimalNullable { get; set; }
+
+        //public DateTime DateTimeNullable { get; set; }
+
+        //public Guid? GuidNullable { get; set; }
+
+        //public bool? BoolNullable { get; set; }
+
+        #endregion
+
+        #region Array of primitives
+        //public int[] IntArray { get; set; }
+
+        //public decimal[] DecimalArray { get; set; }
+
+        //public string[] StringArray { get; set; }
+
+        //public DateTime[] DateTimeArray { get; set; }
+
+        //public Guid[] GuidArray { get; set; }
+
+        //public bool[] BoolArray { get; set; }
+        #endregion
+
+        #region List of primitives
+        //public List<int> IntList { get; set; }
+
+        //public List<decimal> DecimalList { get; set; }
+
+        //public List<string> StringList { get; set; }
+
+        //public List<DateTime> DateTimeList { get; set; }
+
+        //public List<Guid> GuidList { get; set; }
+
+        //public List<bool> BoolList { get; set; }
+        #endregion
+    }
+
+    internal class Cow : BaseClass
+    {
+
+    }
+
+    internal class Mule : BaseClass
+    {
+
+    }
+}
