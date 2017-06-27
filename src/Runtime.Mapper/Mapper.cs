e498ccf7bd434deeb215e1c6c96184ce7b0729e4
aoancea <alx.oancea@gmail.com>
2017-06-27 11:59:45 +0300

    feat(mapper): redo map object expression as an object can be either an array, list, dictionary, primitive type or custom object with properties that have to be mapped independently

--- src/Runtime.Mapper/Mapper.cs
+++ src/Runtime.Mapper/Mapper.cs
@@ -7,117 +7,128 @@ using System.Reflection;
 
 namespace Runtime.Mapper
 {
-    public static class Mapper
-    {
-        private static HashSet<Type> primitiveTypes = new HashSet<Type>(new List<Type>()
-        {
-            typeof(int),    typeof(decimal),    typeof(string),     typeof(Guid),   typeof(DateTime),   typeof(Enum),   typeof(bool),   typeof(char),
-            typeof(int?),   typeof(decimal?),                       typeof(Guid?),  typeof(DateTime?),                  typeof(bool?),  typeof(char?)
-        });
+	public static class Mapper
+	{
+		private static HashSet<Type> primitiveTypes = new HashSet<Type>(new List<Type>()
+		{
+			typeof(int),    typeof(decimal),    typeof(string),     typeof(Guid),   typeof(DateTime),   typeof(Enum),   typeof(bool),   typeof(char),
+			typeof(int?),   typeof(decimal?),                       typeof(Guid?),  typeof(DateTime?),                  typeof(bool?),  typeof(char?)
+		});
 
-        private static ConcurrentDictionary<Tuple<Type, Type>, Func<object, object, object>> mappingFunctions = new ConcurrentDictionary<Tuple<Type, Type>, Func<object, object, object>>();
+		private static ConcurrentDictionary<Tuple<Type, Type>, Func<object, object, object>> mappingFunctions = new ConcurrentDictionary<Tuple<Type, Type>, Func<object, object, object>>();
 
-        public static TDestination DeepCopyTo<TDestination>(this object source)
-        {
-            Type sourceType = source.GetType();
-            Type destinationType = typeof(TDestination);
+		public static TDestination DeepCopyTo<TDestination>(this object source)
+		{
+			Type sourceType = source.GetType();
+			Type destinationType = typeof(TDestination);
 
-            Tuple<Type, Type> key = new Tuple<Type, Type>(sourceType, destinationType);
+			Tuple<Type, Type> key = new Tuple<Type, Type>(sourceType, destinationType);
 
-            Func<object, object, object> mappingFunction = mappingFunctions.GetOrAdd(key, GetMappingFunction(sourceType, destinationType));
+			Func<object, object, object> mappingFunction = mappingFunctions.GetOrAdd(key, GetMappingFunction(sourceType, destinationType));
 
-            return (TDestination)mappingFunction(source, null);
-        }
+			return (TDestination)mappingFunction(source, null);
+		}
 
-        public static void Map<TSource, TDestination>(TSource source, TDestination destination)
-        {
-            Type sourceType = source.GetType();
-            Type destinationType = typeof(TDestination);
+		public static void Map<TSource, TDestination>(TSource source, TDestination destination)
+		{
+			Type sourceType = source.GetType();
+			Type destinationType = typeof(TDestination);
 
-            Tuple<Type, Type> key = new Tuple<Type, Type>(sourceType, destinationType);
+			Tuple<Type, Type> key = new Tuple<Type, Type>(sourceType, destinationType);
 
-            Func<object, object, object> mappingFunction = mappingFunctions.GetOrAdd(key, GetMappingFunction(sourceType, destinationType));
+			Func<object, object, object> mappingFunction = mappingFunctions.GetOrAdd(key, GetMappingFunction(sourceType, destinationType));
 
-            mappingFunction(source, destination);
-        }
+			mappingFunction(source, destination);
+		}
 
 
-        private static Func<object, object, object> GetMappingFunction(Type sourceType, Type destinationType)
-        {
-            ParameterExpression sourceParam = Expression.Parameter(typeof(object), "sourceObj");
-            ParameterExpression destinationParam = Expression.Parameter(typeof(object), "destinationObj");
+		private static Func<object, object, object> GetMappingFunction(Type sourceType, Type destinationType)
+		{
+			ParameterExpression sourceParam = Expression.Parameter(typeof(object), "sourceObj");
+			ParameterExpression destinationParam = Expression.Parameter(typeof(object), "destinationObj");
 
-            List<Expression> expressions = new List<Expression>();
+			List<Expression> expressions = new List<Expression>();
 
-            ParameterExpression sourceVar = Expression.Variable(sourceType, "source");
-            ParameterExpression destinationVar = Expression.Variable(destinationType, "destination");
+			ParameterExpression sourceVar = Expression.Variable(sourceType, "source");
+			ParameterExpression destinationVar = Expression.Variable(destinationType, "destination");
 
-            expressions.Add(Expression.Assign(sourceVar, Expression.Convert(sourceParam, sourceType)));
-            expressions.Add(Expression.Assign(destinationVar, Expression.Convert(Expression.Constant(null), destinationType)));
+			expressions.Add(Expression.Assign(sourceVar, Expression.Convert(sourceParam, sourceType)));
+			expressions.Add(Expression.Assign(destinationVar, Expression.Convert(Expression.Constant(null), destinationType)));
 
-            Expression assignDestinationExpresion = Expression.IfThenElse(Expression.Equal(destinationParam, Expression.Constant(null)),
-                Expression.Assign(destinationVar, Expression.New(destinationType)),
-                Expression.Assign(destinationVar, Expression.Convert(destinationParam, destinationType)));
+			Expression assignDestinationExpresion = Expression.IfThenElse(Expression.Equal(destinationParam, Expression.Constant(null)),
+				Expression.Assign(destinationVar, Expression.New(destinationType)),
+				Expression.Assign(destinationVar, Expression.Convert(destinationParam, destinationType)));
 
-            expressions.Add(assignDestinationExpresion);
+			expressions.Add(assignDestinationExpresion);
 
-            expressions.Add(MapPropertiesExpression(sourceType, destinationType, sourceVar, destinationVar));
+			expressions.AddRange(MapObjectExpression(sourceType, destinationType, sourceVar, destinationVar));
 
-            expressions.Add(destinationVar);
+			expressions.Add(destinationVar);
 
-            LambdaExpression mappingFunctionLambda = Expression.Lambda(Expression.Block(new ParameterExpression[] { sourceVar, destinationVar }, expressions), new ParameterExpression[] { sourceParam, destinationParam });
+			LambdaExpression mappingFunctionLambda = Expression.Lambda(Expression.Block(new ParameterExpression[] { sourceVar, destinationVar }, expressions), new ParameterExpression[] { sourceParam, destinationParam });
 
-            return (Func<object, object, object>)mappingFunctionLambda.Compile();
-        }
+			return (Func<object, object, object>)mappingFunctionLambda.Compile();
+		}
 
-        private static Expression MapPropertiesExpression(Type sourceType, Type destinationType, ParameterExpression sourceVar, ParameterExpression destinationVar)
-        {
-            List<Expression> mapPropertyExpressions = new List<Expression>();
+		private static List<Expression> MapObjectExpression(Type sourceType, Type destinationType, Expression sourceVar, Expression destinationVar)
+		{
+			List<Expression> mapPropertyExpressions = new List<Expression>();
 
-            PropertyInfo[] sourceProperties = sourceType.GetProperties(BindingFlags.Instance | BindingFlags.Public).ToArray();
-            PropertyInfo[] destinationProperties = destinationType.GetProperties(BindingFlags.Instance | BindingFlags.Public).ToArray();
+			if (sourceType.IsArray || destinationType.IsArray || IsGenericList(sourceType) || IsGenericList(destinationType))
+			{
+				// map array or list
+			}
+			else if (IsGenericDictionary(sourceType) || IsGenericDictionary(destinationType))
+			{
+				// map dictionary
+			}
+			else if (primitiveTypes.Contains(sourceType))
+			{
+				// map primitive type => this should not be the case for now :)
 
-            PropertyInfo[] properties = destinationProperties.Where(dp => sourceProperties.Any(sp => sp.Name == dp.Name)).ToArray();
+				mapPropertyExpressions.Add(Expression.Assign(destinationVar, sourceVar));
+			}
+			else
+			{
+				// map object's properties
+				mapPropertyExpressions.AddRange(MapPropertiesExpression(sourceType, destinationType, sourceVar, destinationVar));
+			}
 
-            foreach (PropertyInfo property in properties)
-            {
-                Type sourcePropType = sourceProperties.First(x => x.Name == property.Name).PropertyType;
-                Type destinationPropType = destinationProperties.First(x => x.Name == property.Name).PropertyType;
+			return mapPropertyExpressions;
+		}
 
-                if (sourcePropType.IsArray || destinationPropType.IsArray || IsGenericList(sourcePropType) || IsGenericList(destinationPropType))
-                {
-                    // map array or list
-                }
-                else if (IsGenericDictionary(sourcePropType) || IsGenericDictionary(destinationPropType))
-                {
-                    // map dictionary
-                }
-                else if (primitiveTypes.Contains(sourcePropType))
-                {
-                    // map primitive type => this should not be the case for now :)
 
-                    MemberExpression sourcePropertyAccessorExpression = Expression.Property(sourceVar, property.Name);
-                    MemberExpression destinationPropertyAccessorExpression = Expression.Property(destinationVar, property.Name);
+		private static List<Expression> MapPropertiesExpression(Type sourceType, Type destinationType, Expression sourceVar, Expression destinationVar)
+		{
+			List<Expression> mapPropertyExpressions = new List<Expression>();
 
-                    mapPropertyExpressions.Add(Expression.Assign(destinationPropertyAccessorExpression, sourcePropertyAccessorExpression));
-                }
-                else
-                {
-                    // map custom object's properties
-                }
-            }
+			PropertyInfo[] sourceProperties = sourceType.GetProperties(BindingFlags.Instance | BindingFlags.Public).ToArray();
+			PropertyInfo[] destinationProperties = destinationType.GetProperties(BindingFlags.Instance | BindingFlags.Public).ToArray();
 
-            return Expression.Block(mapPropertyExpressions);
-        }
+			PropertyInfo[] properties = destinationProperties.Where(dp => sourceProperties.Any(sp => sp.Name == dp.Name)).ToArray();
 
-        private static bool IsGenericList(Type type)
-        {
-            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>);
-        }
+			foreach (PropertyInfo property in properties)
+			{
+				Type sourcePropType = sourceProperties.First(x => x.Name == property.Name).PropertyType;
+				Type destinationPropType = destinationProperties.First(x => x.Name == property.Name).PropertyType;
 
-        private static bool IsGenericDictionary(Type type)
-        {
-            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Dictionary<,>);
-        }
-    }
+				MemberExpression sourcePropertyAccessorExpression = Expression.Property(sourceVar, property.Name);
+				MemberExpression destinationPropertyAccessorExpression = Expression.Property(destinationVar, property.Name);
+
+				mapPropertyExpressions.AddRange(MapObjectExpression(sourcePropType, destinationPropType, sourcePropertyAccessorExpression, destinationPropertyAccessorExpression));
+			}
+
+			return mapPropertyExpressions;
+		}
+
+		private static bool IsGenericList(Type type)
+		{
+			return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>);
+		}
+
+		private static bool IsGenericDictionary(Type type)
+		{
+			return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Dictionary<,>);
+		}
+	}
 }
